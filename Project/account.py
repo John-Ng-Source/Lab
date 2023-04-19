660a09e6687696bd61020d1bbc893e6eb599d18b
John-Ng-Source <80951267+John-Ng-Source@users.noreply.github.com>
2023-04-19 15:12:44 -0500

    Docstrings and type hinting - implemented

--- Project/account.py
+++ Project/account.py
@@ -1,10 +1,15 @@
 
 class Account:
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
+        """Constructor to create initial state of account object
+           :param name: Person's First or Full Name."""
         self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount: float) -> bool:
+        """Method to positively increment account balance.
+        :param amount: Deposit amount
+        :return: Success of Method - True or False"""
         success = False
         if self.__account_balance <= 0:
             return success
@@ -13,8 +18,11 @@ class Account:
             success = True
             return success
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float):
         success = False
+        """Method to negatively increment account balance.
+           :param amount: deposit amount.
+           :return: Success of Method - True or False"""
         if self.__account_balance <= 0:
             return success
         else:
@@ -22,10 +30,14 @@ class Account:
             success = True
             return success
 
-    def get_balance(self):
+    def get_balance(self) -> float:
+        """Method to return account balance.
+           :return: account balance."""
         return self.__account_balance
 
-    def get_name(self):
+    def get_name(self) -> str:
+        """Method to return account name.
+           :return: account name."""
         return self.__account_name
 
     
