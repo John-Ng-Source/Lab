468faa0b7013bbee71965980764a71917796938d
John-Ng-Source <80951267+John-Ng-Source@users.noreply.github.com>
2023-04-19 13:57:24 -0500

    Class Account Creation

--- Project/account.py
+++ Project/account.py
@@ -1,1 +1,22 @@
 
+class Account:
+    def __init__(self, name):
+        self.__account_name = name
+        self.__account_balance = 0
+
+    def deposit(self, amount):
+        success = False
+        if self.__account_balance <= 0:
+            return success
+        else:
+            self.__account_balance += amount
+            success = True
+            return success
+
+    def get_balance(self):
+        return self.__account_balance
+
+    def get_name(self):
+        return self.__account_name
+
+    
